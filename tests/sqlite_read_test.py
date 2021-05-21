dbede3350ac327d2b176e17c48ff7e8f33cd60d0
Tom Stowers <tom.stowers@esure.com>
2021-05-21 13:56:11 +0100

    added additional tests for all tables existing in db

--- tests/sqlite_read_test.py
+++ tests/sqlite_read_test.py
@@ -1,9 +1,33 @@
 import sqlite3
 
 
-def test_sqlite_read():
+def test_sqlite_read_policy():
     con = sqlite3.connect("sqlite.db")
     cur = con.cursor()
     cur.execute("SELECT * FROM policy")
     rows = cur.fetchall()
     assert len(rows) == 49
+
+
+def test_sqlite_read_finance():
+    con = sqlite3.connect("sqlite.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM finance")
+    rows = cur.fetchall()
+    assert len(rows) == 49
+
+
+def test_sqlite_read_calendar():
+    con = sqlite3.connect("sqlite.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM calendar")
+    rows = cur.fetchall()
+    assert len(rows) == 389
+
+
+def test_sqlite_read_policy_lifecycle():
+    con = sqlite3.connect("sqlite.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM policy_lifecycle")
+    rows = cur.fetchall()
+    assert len(rows) == 127
